81b8a68cf32b1eff99b760cb46e2749373352bb0
subrat <221b397@juetguna.in>
2025-01-16 12:02:53 +0530

    Update LEO.java

--- LEO.java
+++ LEO.java
@@ -8,7 +8,28 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 public class Main
 {
+    String name;
+    int age;
+    
+    void setName(String name){
+        this.name=name;
+    }
+     void setAge(int age){
+        this.age=age;
+    }
+    
+    String getName(){
+        return this.name;
+    }
+    int getAge(){
+        return this.age;}
+    
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+	Main m1=new Main();
+	m1.setName("subrat");
+	m1.setAge(22);
+	System.out.println(m1.getName());
+	System.out.println(m1.getAge());
+
 	}
 }
